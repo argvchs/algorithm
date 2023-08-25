5521d676368d135992275d6eaf658286525f295b
argvchs <argvchs@outlook.com>
2023-08-25 22:26:18 +0800

    style: format code

--- graph/max-flow/hlpp-optimized.cpp
+++ graph/max-flow/hlpp-optimized.cpp
@@ -7,8 +7,8 @@ using namespace std;
 using i64 = long long;
 const int N = 1205, INF32 = 0x3f3f3f3f;
 const i64 INF64 = 0x3f3f3f3f3f3f3f3f;
-int n, m, s, t, h[N], gap[N << 1], top;
-i64 e[N], ans;
+int n, m, s, t, dep[N], gap[N << 1], top;
+i64 ans[N];
 bool vis[N];
 vector<tuple<int, i64, int>> G[N];
 queue<int> Q;
@@ -18,60 +18,59 @@ void addflow(int u, int v, i64 w) {
     G[v].emplace_back(u, 0, G[u].size() - 1);
 }
 bool bfs() {
-    memset(h, 0x3f, sizeof(h));
-    h[t] = 0, vis[t] = true;
+    memset(dep, 0x3f, sizeof(dep));
+    dep[t] = 0, vis[t] = true;
     Q.push(t);
     while (!Q.empty()) {
         int u = Q.front();
         Q.pop();
         for (auto [v, w, rev] : G[u])
             if (!vis[v] && get<1>(G[v][rev])) {
-                h[v] = h[u] + 1, vis[v] = true;
+                dep[v] = dep[u] + 1, vis[v] = true;
                 Q.push(v);
             }
     }
-    return h[s] != INF32;
+    return dep[s] != INF32;
 }
 bool push(int u) {
     for (auto &[v, w, rev] : G[u])
-        if (vis[v] && (u == s || h[u] == h[v] + 1) && w) {
-            i64 flow = min(e[u], w);
-            if (v != s && v != t && !e[v]) {
-                top = max(top, h[v]);
-                S[h[v]].push(v);
+        if (vis[v] && (u == s || dep[u] == dep[v] + 1) && w) {
+            i64 flow = min(ans[u], w);
+            if (v != s && v != t && !ans[v]) {
+                top = max(top, dep[v]);
+                S[dep[v]].push(v);
             }
-            e[u] -= flow, e[v] += flow, w -= flow, get<1>(G[v][rev]) += flow;
-            if (!e[u]) return false;
+            ans[u] -= flow, ans[v] += flow, w -= flow, get<1>(G[v][rev]) += flow;
+            if (!ans[u]) return false;
         }
     return true;
 }
 void relabel(int u) {
-    h[u] = INF32;
+    dep[u] = INF32;
     for (auto [v, w, rev] : G[u])
-        if (vis[v] && w) h[u] = min(h[u], h[v] + 1);
-    if (h[u] < n) {
-        top = max(top, h[u]), ++gap[h[u]];
-        S[h[u]].push(u);
+        if (vis[v] && w) dep[u] = min(dep[u], dep[v] + 1);
+    if (dep[u] < n) {
+        top = max(top, dep[u]), ++gap[dep[u]];
+        S[dep[u]].push(u);
     }
 }
 void hlpp() {
     if (!bfs()) return;
-    h[s] = n, e[s] = INF64;
+    dep[s] = n, ans[s] = INF64;
     for (int i = 1; i <= n; i++)
-        if (vis[i]) ++gap[h[i]];
+        if (vis[i]) ++gap[dep[i]];
     push(s);
     while (top) {
         int u = S[top].top();
         S[top].pop();
         if (push(u)) {
-            if (!--gap[h[u]])
+            if (!--gap[dep[u]])
                 for (int i = 1; i <= n; i++)
-                    if (vis[i] && i != s && h[i] > h[u]) h[i] = n + 1;
+                    if (vis[i] && i != s && dep[i] > dep[u]) dep[i] = n + 1;
             relabel(u);
         }
         while (!S[top].size() && top) --top;
     }
-    ans = e[t];
 }
 int main() {
     ios::sync_with_stdio(false);
@@ -82,6 +81,6 @@ int main() {
         addflow(u, v, w);
     }
     hlpp();
-    cout << ans;
+    cout << ans[t];
     return 0;
 }
--- graph/max-flow/hlpp.cpp
+++ graph/max-flow/hlpp.cpp
@@ -6,8 +6,8 @@ using namespace std;
 using i64 = long long;
 const int N = 1205, INF32 = 0x3f3f3f3f;
 const i64 INF64 = 0x3f3f3f3f3f3f3f3f;
-int n, m, s, t, h[N], gap[N << 1];
-i64 e[N], ans;
+int n, m, s, t, dep[N], gap[N << 1];
+i64 ans[N];
 bool vis[N];
 vector<tuple<int, i64, int>> G[N];
 queue<int> Q;
@@ -17,56 +17,55 @@ void addflow(int u, int v, i64 w) {
     G[v].emplace_back(u, 0, G[u].size() - 1);
 }
 bool bfs() {
-    memset(h, 0x3f, sizeof(h));
-    h[t] = 0, vis[t] = true;
+    memset(dep, 0x3f, sizeof(dep));
+    dep[t] = 0, vis[t] = true;
     Q.push(t);
     while (!Q.empty()) {
         int u = Q.front();
         Q.pop();
         for (auto [v, w, rev] : G[u])
             if (!vis[v] && get<1>(G[v][rev])) {
-                h[v] = h[u] + 1, vis[v] = true;
+                dep[v] = dep[u] + 1, vis[v] = true;
                 Q.push(v);
             }
     }
-    return h[s] != INF32;
+    return dep[s] != INF32;
 }
 bool push(int u) {
     for (auto &[v, w, rev] : G[u])
-        if (vis[v] && (u == s || h[u] == h[v] + 1) && w) {
-            i64 flow = min(e[u], w);
-            if (v != s && v != t && !e[v]) PQ.emplace(h[v], v);
-            e[u] -= flow, e[v] += flow, w -= flow, get<1>(G[v][rev]) += flow;
-            if (!e[u]) return false;
+        if (vis[v] && (u == s || dep[u] == dep[v] + 1) && w) {
+            i64 flow = min(ans[u], w);
+            if (v != s && v != t && !ans[v]) PQ.emplace(dep[v], v);
+            ans[u] -= flow, ans[v] += flow, w -= flow, get<1>(G[v][rev]) += flow;
+            if (!ans[u]) return false;
         }
     return true;
 }
 void relabel(int u) {
-    h[u] = INF32;
+    dep[u] = INF32;
     for (auto [v, w, rev] : G[u])
-        if (vis[v] && w) h[u] = min(h[u], h[v] + 1);
-    if (h[u] < n) {
-        ++gap[h[u]];
-        PQ.emplace(h[u], u);
+        if (vis[v] && w) dep[u] = min(dep[u], dep[v] + 1);
+    if (dep[u] < n) {
+        ++gap[dep[u]];
+        PQ.emplace(dep[u], u);
     }
 }
 void hlpp() {
     if (!bfs()) return;
-    h[s] = n, e[s] = INF64;
+    dep[s] = n, ans[s] = INF64;
     for (int i = 1; i <= n; i++)
-        if (vis[i]) ++gap[h[i]];
+        if (vis[i]) ++gap[dep[i]];
     push(s);
     while (!PQ.empty()) {
         int u = PQ.top().second;
         PQ.pop();
         if (push(u)) {
-            if (!--gap[h[u]])
+            if (!--gap[dep[u]])
                 for (int i = 1; i <= n; i++)
-                    if (vis[i] && i != s && h[i] > h[u]) h[i] = n + 1;
+                    if (vis[i] && i != s && dep[i] > dep[u]) dep[i] = n + 1;
             relabel(u);
         }
     }
-    ans = e[t];
 }
 int main() {
     ios::sync_with_stdio(false);
@@ -77,6 +76,6 @@ int main() {
         addflow(u, v, w);
     }
     hlpp();
-    cout << ans;
+    cout << ans[t];
     return 0;
 }
