e4e59aba606f770c5db8b9b117533e1c6f756c58
argvchs <argvchs@outlook.com>
2023-08-28 19:24:08 +0800

    feat: update graph/shortest-path/floyd

--- graph/shortest-path/floyd.cpp
+++ graph/shortest-path/floyd.cpp
@@ -1,29 +1,24 @@
 #include <cstring>
 #include <iostream>
-#include <queue>
 using namespace std;
-using i64 = long long;
 const int N = 105;
 int n, m, dis[N][N];
-i64 ans;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-            if (i != j) dis[i][j] = 1e9;
+    memset(dis, 0x3f, sizeof(dis));
+    for (int i = 1; i <= n; i++) dis[i][i] = 0;
     for (int i = 1, u, v, w; i <= m; i++) {
         cin >> u >> v >> w;
-        dis[u][v] = min(dis[u][v], w);
+        dis[u][v] = dis[v][u] = min(dis[u][v], w);
     }
     for (int k = 1; k <= n; k++)
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++) dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
     for (int i = 1; i <= n; i++) {
-        ans = 0;
-        for (int j = 1; j <= n; j++) ans += (i64)j * dis[i][j];
-        cout << ans << '\n';
+        for (int j = 1; j <= n; j++) cout << dis[i][j] << ' ';
+        cout << '\n';
     }
     return 0;
 }
