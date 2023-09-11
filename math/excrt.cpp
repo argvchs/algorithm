eae16f3e9095aeb4b30bff4d1b0ba9b25b50e49e
argvchs <argvchs@outlook.com>
2023-09-11 18:49:00 +0800

    fix: update math/excrt

--- math/excrt.cpp
+++ math/excrt.cpp
@@ -2,6 +2,7 @@
 #include <tuple>
 using namespace std;
 using i64 = long long;
+using i128 = __int128;
 int n;
 i64 a, p = 1, ai, pi;
 tuple<i64, i64, i64> exgcd(i64 a, i64 b) {
@@ -11,8 +12,8 @@ tuple<i64, i64, i64> exgcd(i64 a, i64 b) {
 }
 pair<i64, i64> excrt(i64 a1, i64 p1, i64 a2, i64 p2) {
     auto [x, y, gcd] = exgcd(p1, p2);
-    i64 lcm = p1 * p2 / gcd;
-    x = (x * (a2 - a1) / gcd + p2 / gcd) % (p2 / gcd);
+    i64 lcm = (i128)p1 * p2 / gcd;
+    x = ((i128)x * (a2 - a1) / gcd + p2 / gcd) % (p2 / gcd);
     return {(p1 * x + a1 + lcm) % lcm, lcm};
 }
 int main() {
