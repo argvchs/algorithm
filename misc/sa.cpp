c12835ff18a29e204295aab3b2bd171e2259c936
argvchs <argvchs@outlook.com>
2023-09-12 19:15:47 +0800

    feat: optimize misc/sa

--- misc/sa.cpp
+++ misc/sa.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 using f80 = long double;
 const int N = 1005;
-const f80 A = 0.99, EPS = 1e-14;
+const f80 A = 0.99, EPS = 1e-7;
 int n, a[N], b[N], c[N];
-f80 ansx, ansy, dis = 1e18;
+f80 ansx, ansy, ansz = __builtin_infl();
 mt19937 rng(random_device{}());
 uniform_real_distribution<f80> dist;
 f80 calc(f80 x, f80 y) {
@@ -14,12 +14,12 @@ f80 calc(f80 x, f80 y) {
         f80 nx = x - a[i], ny = y - b[i];
         ret += (sqrt(nx * nx + ny * ny)) * c[i];
     }
-    if (ret < dis) ansx = x, ansy = y, dis = ret;
+    if (ret < ansz) ansx = x, ansy = y, ansz = ret;
     return ret;
 }
 void solve() {
     f80 x = ansx, y = ansy;
-    for (f80 t = 1e4; t > EPS; t *= A) {
+    for (f80 t = 1000; t > EPS; t *= A) {
         f80 nx = ansx + (dist(rng) * 2 - 1) * t;
         f80 ny = ansy + (dist(rng) * 2 - 1) * t;
         f80 delta = calc(nx, ny) - calc(x, y);
@@ -31,7 +31,7 @@ int main() {
     cin.tie(nullptr);
     cin >> n;
     for (int i = 1; i <= n; i++) cin >> a[i] >> b[i] >> c[i];
-    for (int i = 1; i <= 5; i++) solve();
+    for (int i = 1; i <= 3; i++) solve();
     cout.setf(ios::fixed);
     cout.precision(3);
     cout << ansx << ' ' << ansy;
