2787fa6522d2e3194d0a258ccf04b0bf81f500bc
argvchs <argvchs@outlook.com>
2023-12-15 01:24:43 +0800

    feat: change bound/dinic

--- graph/network-flow/bound/dinic.cpp
+++ graph/network-flow/bound/dinic.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-const int N = 2e3 + 5, M = 1e6 + 5, INF = 0x3f3f3f3f;
-int n, m, s, t, ss, tt, a[N], dep[N], cur[N], head[N], cnt, sum, ans;
+const int N = 205, M = 2e4 + 5, INF = 0x3f3f3f3f;
+int n, m, s, t, ss, tt, a[N], dep[N], cur[N], head[N], cnt = 1, sum, ans;
 bool vis[N];
 struct edge {
     int to, nxt, w;
@@ -11,7 +11,6 @@ struct edge {
 queue<int> Q;
 void add(int u, int v, int w) { e[++cnt] = {v, head[u], w}, head[u] = cnt; }
 void addflow(int u, int v, int w) { add(u, v, w), add(v, u, 0); }
-void addflow(int u, int v, int l, int r) { a[u] -= l, a[v] += l, addflow(u, v, r - l); }
 bool bfs() {
     memset(dep, 0x3f, sizeof(dep));
     memset(vis, 0, sizeof(vis));
@@ -49,28 +48,19 @@ void dinic() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    while (cin >> n >> m) {
-        memset(head, 0, sizeof(head));
-        memset(a, 0, sizeof(a));
-        cnt = 1, sum = ans = 0;
-        s = n + m + 1, t = s + 1, ss = s + 2, tt = s + 3;
-        for (int i = 1, x; i <= m; i++) cin >> x, addflow(i + n, t, x, INF);
-        for (int i = 1, x, y, z, l, r; i <= n; i++) {
-            cin >> x >> y, addflow(s, i, y);
-            for (int j = 1; j <= x; j++) cin >> z >> l >> r, addflow(i, z + n + 1, l, r);
-        }
-        for (int i = 1; i <= t; i++)
-            if (a[i] > 0) addflow(ss, i, a[i]), sum += a[i];
-            else addflow(i, tt, -a[i]);
-        addflow(t, s, INF);
-        swap(s, ss), swap(t, tt), dinic();
-        if (ans != sum) {
-            cout << "-1\n\n";
-            continue;
-        }
-        sum = e[cnt].w, ans = e[cnt].w = e[cnt ^ 1].w = 0;
-        swap(s, ss), swap(t, tt), dinic();
-        cout << sum + ans << "\n\n";
+    cin >> n >> m >> s >> t, ss = n + 1, tt = n + 2;
+    for (int i = 1, u, v, l, r; i <= m; i++) {
+        cin >> u >> v >> l >> r;
+        a[u] -= l, a[v] += l, addflow(u, v, r - l);
     }
+    for (int i = 1; i <= n; i++)
+        if (a[i] > 0) addflow(ss, i, a[i]), sum += a[i];
+        else addflow(i, tt, -a[i]);
+    addflow(t, s, INF);
+    swap(s, ss), swap(t, tt), dinic();
+    if (ans != sum) return cout << "please go home to sleep", 0;
+    sum = e[cnt].w, ans = e[cnt].w = e[cnt ^ 1].w = 0;
+    swap(s, ss), swap(t, tt), dinic();
+    cout << sum + ans;
     return 0;
 }
