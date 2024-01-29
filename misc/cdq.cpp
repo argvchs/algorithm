d34b8b2bd756ebc51a0def1e7bbf7a702bca799c
argvchs <argvchs@outlook.com>
2024-01-29 22:51:53 +0800

    cdq

--- misc/cdq.cpp
+++ misc/cdq.cpp
@@ -6,12 +6,13 @@ int n, m, t[N], ans[N];
 struct node {
     int a, b, c, cnt, ans;
 } a[N], b[N];
-bool operator<(node x, node y) {
-    if (x.a != y.a) return x.a < y.a;
-    if (x.b != y.b) return x.b < y.b;
-    return x.c < y.c;
+bool cmp1(const node &x, const node &y) {
+    return tie(x.a, x.b, x.c) < tie(y.a, y.b, y.c);
 }
-bool operator==(node x, node y) { return !(x < y || y < x); }
+bool cmp2(const node &x, const node &y) {
+    return tie(x.b, x.c, x.a) < tie(y.b, y.c, y.a);
+}
+bool operator==(const node &x, const node &y) { return !cmp1(x, y) && !cmp1(y, x); }
 void update(int x, int k) {
     for (int i = x; i <= m; i += i & -i) t[i] += k;
 }
@@ -24,25 +25,24 @@ void cdq(int l, int r) {
     if (l == r) return;
     int mid = (l + r) >> 1;
     cdq(l, mid), cdq(mid + 1, r);
-    int i = l, j = mid + 1, cnt = 0;
-    while (i <= mid && j <= r)
-        if (a[i].b <= a[j].b) update(a[i].c, a[i].cnt), b[++cnt] = a[i++];
-        else a[j].ans += query(a[j].c), b[++cnt] = a[j++];
-    while (i <= mid) update(a[i].c, a[i].cnt), b[++cnt] = a[i++];
-    while (j <= r) a[j].ans += query(a[j].c), b[++cnt] = a[j++];
-    for (int i = l; i <= mid; i++) update(a[i].c, -a[i].cnt);
-    for (int i = 1; i <= cnt; i++) a[i + l - 1] = b[i];
+    sort(a + l, a + r + 1, cmp2);
+    for (int i = l; i <= r; i++)
+        if (a[i].a <= mid) update(a[i].c, a[i].cnt);
+        else a[i].ans += query(a[i].c);
+    for (int i = l; i <= r; i++)
+        if (a[i].a <= mid) update(a[i].c, -a[i].cnt);
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
     for (int i = 1; i <= n; i++) cin >> a[i].a >> a[i].b >> a[i].c;
-    sort(a + 1, a + n + 1);
+    sort(a + 1, a + n + 1, cmp1);
     for (int i = 1; i <= n; i++) b[i] = a[i];
     int cnt = unique(a + 1, a + n + 1) - a - 1;
     for (int i = 1, j = 1; i <= cnt; i++)
         while (j <= n && a[i] == b[j]) ++j, ++a[i].cnt;
+    for (int i = 1; i <= cnt; i++) a[i].a = i;
     cdq(1, cnt);
     for (int i = 1; i <= cnt; i++) ans[a[i].ans + a[i].cnt - 1] += a[i].cnt;
     for (int i = 0; i <= n - 1; i++) cout << ans[i] << '\n';
