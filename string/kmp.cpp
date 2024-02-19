704b3726d6c31e1d8eeb01a601a5706285e053e3
argvchs <argvchs@outlook.com>
2024-02-19 14:45:02 +0800

    kmp nxt

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 const int N = 1e6 + 5;
-int n, m, a[N], b[N], fail[N];
+int n, m, a[N], b[N], nxt[N];
 string s, t;
 int main() {
     ios_base::sync_with_stdio(false);
@@ -11,14 +11,14 @@ int main() {
     for (char c : s) a[++n] = c - 'A';
     for (char c : t) b[++m] = c - 'A';
     for (int i = 2, j = 0; i <= m; i++) {
-        while (j && b[i] != b[j + 1]) j = fail[j];
+        while (j && b[i] != b[j + 1]) j = nxt[j];
         if (b[i] == b[j + 1]) ++j;
-        fail[i] = j;
+        nxt[i] = j;
     }
     for (int i = 1, j = 0; i <= n; i++) {
-        while (j && a[i] != b[j + 1]) j = fail[j];
+        while (j && a[i] != b[j + 1]) j = nxt[j];
         if (a[i] == b[j + 1]) ++j;
-        if (j == m) cout << i - j + 1 << '\n', j = fail[j];
+        if (j == m) cout << i - j + 1 << '\n', j = nxt[j];
     }
-    for (int i = 1; i <= m; i++) cout << fail[i] << ' ';
+    for (int i = 1; i <= m; i++) cout << nxt[i] << ' ';
 }
