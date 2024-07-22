91a9538ced5f2ae6d460617e60605188c6b7e1b9
argvchs <argvchs@outlook.com>
2024-07-23 02:48:27 +0800

    feat: optimize poly

--- math/poly/fft.cpp
+++ math/poly/fft.cpp
@@ -7,35 +7,39 @@ using f64 = double;
 using c64 = complex<f64>;
 const int N = 3e6 + 5;
 const f64 PI = acos(-1);
-int n, m, r[N], lim = 1, cnt;
-c64 f[N], g[N], w[N];
-void fft(c64 f[], int t) {
-    for (int i = 0; i < lim; i++)
-        if (i < r[i]) swap(f[i], f[r[i]]);
-    for (int i = 1; i < lim; i <<= 1)
-        for (int j = 0; j < lim; j += i << 1)
+int n, m, lim = 1;
+c64 a[N], b[N], w[N];
+void dit(c64 a[], int n) {
+    for (int i = 1; i < n; i <<= 1)
+        for (int j = 0; j < n; j += i << 1)
             for (int k = 0; k < i; k++) {
-                c64 x = f[j + k], y = f[j + k + i] * w[i + k];
-                f[j + k] = x + y, f[j + k + i] = x - y;
+                c64 x = a[j + k], y = a[j + k + i] * w[i + k];
+                a[j + k] = x + y, a[j + k + i] = x - y;
             }
-    if (t == 1) return;
-    reverse(f + 1, f + lim);
-    for (int i = 0; i < lim; i++) f[i] /= lim;
+    reverse(a + 1, a + n);
+    for (int i = 0; i < n; i++) a[i] /= n;
+}
+void dif(c64 a[], int n) {
+    for (int i = n >> 1; i; i >>= 1)
+        for (int j = 0; j < n; j += i << 1)
+            for (int k = 0; k < i; k++) {
+                c64 x = a[j + k], y = a[j + k + i];
+                a[j + k] = x + y, a[j + k + i] = (x - y) * w[i + k];
+            }
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    for (int i = 0; i <= n; i++) cin >> f[i];
-    for (int i = 0; i <= m; i++) cin >> g[i];
-    while (lim <= n + m) lim <<= 1, ++cnt;
+    for (int i = 0; i <= n; i++) cin >> a[i];
+    for (int i = 0; i <= m; i++) cin >> b[i];
+    while (lim <= n + m) lim <<= 1;
     c64 wn = exp(PI / (lim >> 1) * 1i);
     w[lim >> 1] = 1;
     for (int i = (lim >> 1) + 1; i < lim; i++) w[i] = w[i - 1] * wn;
-    for (int i = (lim >> 1) - 1; i > 0; i--) w[i] = w[i << 1];
-    for (int i = 0; i < lim; i++) r[i] = r[i >> 1] >> 1 | (i & 1) << (cnt - 1);
-    fft(f, 1), fft(g, 1);
-    for (int i = 0; i < lim; i++) f[i] *= g[i];
-    fft(f, -1);
-    for (int i = 0; i <= n + m; i++) cout << (int)round(f[i].real()) << ' ';
+    for (int i = (lim >> 1) - 1; i; i--) w[i] = w[i << 1];
+    dif(a, lim), dif(b, lim);
+    for (int i = 0; i < lim; i++) a[i] *= b[i];
+    dit(a, lim);
+    for (int i = 0; i <= n + m; i++) cout << (int)round(a[i].real()) << ' ';
 }
--- math/poly/ntt.cpp
+++ math/poly/ntt.cpp
@@ -3,41 +3,45 @@
 using namespace std;
 using i64 = long long;
 const int N = 3e6 + 5, P = 998244353;
-int n, m, f[N], g[N], r[N], w[N], lim = 1, cnt;
+int n, m, a[N], b[N], w[N], lim = 1;
 int qpow(int a, int b) {
     int ret = 1;
     for (; b; b >>= 1, a = (i64)a * a % P)
         if (b & 1) ret = (i64)ret * a % P;
     return ret;
 }
-void ntt(int f[], int t) {
-    for (int i = 0; i < lim; i++)
-        if (i < r[i]) swap(f[i], f[r[i]]);
-    for (int i = 1; i < lim; i <<= 1)
-        for (int j = 0; j < lim; j += i << 1)
+void dit(int *a, int n) {
+    for (int i = 1; i < n; i <<= 1)
+        for (int j = 0; j < n; j += i << 1)
             for (int k = 0; k < i; k++) {
-                int x = f[j + k], y = (i64)f[j + k + i] * w[i + k] % P;
-                f[j + k] = (x + y) % P, f[j + k + i] = (x - y + P) % P;
+                int x = a[j + k], y = (i64)a[j + k + i] * w[i + k] % P;
+                a[j + k] = (x + y) % P, a[j + k + i] = (x - y + P) % P;
             }
-    if (t == 1) return;
-    reverse(f + 1, f + lim);
-    int inv = qpow(lim, P - 2);
-    for (int i = 0; i < lim; i++) f[i] = (i64)f[i] * inv % P;
+    int inv = qpow(n, P - 2);
+    reverse(a + 1, a + n);
+    for (int i = 0; i < n; i++) a[i] = (i64)a[i] * inv % P;
+}
+void dif(int *a, int n) {
+    for (int i = n >> 1; i; i >>= 1)
+        for (int j = 0; j < n; j += i << 1)
+            for (int k = 0; k < i; k++) {
+                int x = a[j + k], y = a[j + k + i];
+                a[j + k] = (x + y) % P, a[j + k + i] = (i64)(x - y + P) * w[i + k] % P;
+            }
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    for (int i = 0; i <= n; i++) cin >> f[i];
-    for (int i = 0; i <= m; i++) cin >> g[i];
-    while (lim <= n + m) lim <<= 1, ++cnt;
-    int wn = qpow(3, (P - 1) >> cnt);
+    for (int i = 0; i <= n; i++) cin >> a[i];
+    for (int i = 0; i <= m; i++) cin >> b[i];
+    while (lim <= n + m) lim <<= 1;
+    int wn = qpow(3, (P - 1) / lim);
     w[lim >> 1] = 1;
     for (int i = (lim >> 1) + 1; i < lim; i++) w[i] = (i64)w[i - 1] * wn % P;
-    for (int i = (lim >> 1) - 1; i > 0; i--) w[i] = w[i << 1];
-    for (int i = 0; i < lim; i++) r[i] = r[i >> 1] >> 1 | (i & 1) << (cnt - 1);
-    ntt(f, 1), ntt(g, 1);
-    for (int i = 0; i < lim; i++) f[i] = (i64)f[i] * g[i] % P;
-    ntt(f, -1);
-    for (int i = 0; i <= n + m; i++) cout << f[i] << ' ';
+    for (int i = (lim >> 1) - 1; i; i--) w[i] = w[i << 1];
+    dif(a, lim), dif(b, lim);
+    for (int i = 0; i < lim; i++) a[i] = (i64)a[i] * b[i] % P;
+    dit(a, lim);
+    for (int i = 0; i <= n + m; i++) cout << a[i] << ' ';
 }
