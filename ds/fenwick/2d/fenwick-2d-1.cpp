6aaf882dbd1563725a3808aca207385c18c0f7ca
argvchs <argvchs@outlook.com>
2024-12-28 09:50:15 +0800

    feat: loj bit

--- ds/fenwick/2d/fenwick-2d-1.cpp
+++ ds/fenwick/2d/fenwick-2d-1.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
-const int N = 3e3 + 5;
-int n, m, q, t[N][N];
-void update(int x, int y, int k) {
+using i64 = long long;
+const int N = 5e3 + 5;
+int n, m;
+i64 t[N][N];
+void update(int x, int y, i64 k) {
     for (int i = x; i <= n; i += i & -i)
         for (int j = y; j <= m; j += j & -j) t[i][j] += k;
 }
-int query(int x, int y) {
-    int ret = 0;
+i64 query(int x, int y) {
+    i64 ret = 0;
     for (int i = x; i >= 1; i -= i & -i)
         for (int j = y; j >= 1; j -= j & -j) ret += t[i][j];
     return ret;
 }
-int query(int x, int y, int z, int w) {
+i64 query(int x, int y, int z, int w) {
     return query(z, w) - query(x - 1, w) - query(z, y - 1) + query(x - 1, y - 1);
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> n >> m >> q;
-    for (int i = 1, op, x, y, z, w; i <= q; i++) {
-        cin >> op >> x >> y >> z;
+    cin >> n >> m;
+    for (int op, x, y, z, w; cin >> op >> x >> y >> z;)
         if (op == 1) update(x, y, z);
         else cin >> w, cout << query(x, y, z, w) << '\n';
-    }
 }
--- ds/fenwick/2d/fenwick-2d-3.cpp
+++ ds/fenwick/2d/fenwick-2d-3.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
-const int N = 3e3 + 5;
-int n, m, q, t1[N][N], t2[N][N], t3[N][N], t4[N][N];
-void update(int x, int y, int k) {
+using i64 = long long;
+const int N = 5e3 + 5;
+int n, m;
+i64 t1[N][N], t2[N][N], t3[N][N], t4[N][N];
+void update(int x, int y, i64 k) {
     for (int i = x; i <= n; i += i & -i)
-        for (int j = y; j <= n; j += j & -j) {
+        for (int j = y; j <= m; j += j & -j) {
             t1[i][j] += k;
             t2[i][j] += k * x;
             t3[i][j] += k * y;
             t4[i][j] += k * x * y;
         }
 }
-void update(int x, int y, int z, int w, int k) {
+void update(int x, int y, int z, int w, i64 k) {
     update(x, y, k);
     update(x, w + 1, -k);
     update(z + 1, y, -k);
     update(z + 1, w + 1, k);
 }
-int query(int x, int y) {
-    int ret = 0;
+i64 query(int x, int y) {
+    i64 ret = 0;
     for (int i = x; i >= 1; i -= i & -i)
         for (int j = y; j >= 1; j -= j & -j) {
             ret += (x + 1) * (y + 1) * t1[i][j];
@@ -28,16 +30,14 @@ int query(int x, int y) {
         }
     return ret;
 }
-int query(int x, int y, int z, int w) {
+i64 query(int x, int y, int z, int w) {
     return query(z, w) - query(x - 1, w) - query(z, y - 1) + query(x - 1, y - 1);
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> n >> m >> q;
-    for (int i = 1, op, x, y, z, w, k; i <= q; i++) {
-        cin >> op >> x >> y >> z >> w;
+    cin >> n >> m;
+    for (int op, x, y, z, w, k; cin >> op >> x >> y >> z >> w;)
         if (op == 1) cin >> k, update(x, y, z, w, k);
         else cout << query(x, y, z, w) << '\n';
-    }
 }
