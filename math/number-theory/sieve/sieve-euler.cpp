a7fd008a927c2892265ee03aedc0e9a7abebe3c2
argvchs <argvchs@outlook.com>
2025-02-16 00:12:35 +0800

    feat: update sieve-euler

--- math/number-theory/sieve/sieve-euler.cpp
+++ math/number-theory/sieve/sieve-euler.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-using i64 = long long;
 const int N = 4e4 + 5;
 int n, p[N], phi[N], cnt, ans;
 bool vis[N];
@@ -8,7 +7,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n, phi[1] = 1;
-    if (n == 1) return cout << '0', 0;
+    if (!--n) return cout << '0', 0;
     for (int i = 2; i <= n; i++) {
         if (!vis[i]) p[++cnt] = i, phi[i] = i - 1;
         for (int j = 1; j <= cnt && i * p[j] <= n; j++) {
@@ -17,6 +16,6 @@ int main() {
             if (!(i % p[j])) break;
         }
     }
-    for (int i = 1; i <= n - 1; i++) ans += phi[i];
+    for (int i = 1; i <= n; i++) ans += phi[i];
     cout << (ans << 1 | 1);
 }
